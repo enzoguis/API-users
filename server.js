9a4321d5ce9e34509e98e9933a12ade9976e49af
enzoguis <enzog0315@gmail.com>
2024-09-14 19:49:28 -0300

    add/ajuste CRUD

--- server.js
+++ server.js
@@ -6,8 +6,6 @@ const prisma = new PrismaClient()
 const app = express()
 app.use(express.json())
 
-const users = []
-
 app.post('/usuarios', async (req, res) => {
   await prisma.user.create({
     data: {
@@ -20,7 +18,32 @@ app.post('/usuarios', async (req, res) => {
   res.status(201).json(req.body)
 })
 
-app.get('/usuarios', (req, res) => {
+app.put('/usuarios/:id', async (req, res) => {
+  await prisma.user.update({
+    where: {
+      id: req.params.id,
+    },
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age,
+    },
+  })
+  res.status(201).json(req.body)
+})
+
+app.delete('/usuarios/:id', async (req, res) => {
+  await prisma.user.delete({
+    where: {
+      id: req.params.id,
+    },
+  })
+  res.status(200).json({ message: 'Usuário deletado!' })
+})
+
+app.get('/usuarios', async (req, res) => {
+  const users = await prisma.user.findMany()
+
   res.status(200).json(users)
 })
 
